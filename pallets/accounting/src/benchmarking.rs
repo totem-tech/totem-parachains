13baadba91bd9d25a313d472205afa2646216dee
Damilare <dharjeezy@gmail.com>
2023-03-31 23:44:21 +0300

    started benchmarking

--- pallets/accounting/src/benchmarking.rs
+++ pallets/accounting/src/benchmarking.rs
@@ -5,17 +5,42 @@
 use super::*;
 
 #[allow(unused)]
-use crate::Pallet as Template;
+use crate::Pallet as Accounting;
 use frame_benchmarking::{benchmarks, impl_benchmark_test_suite, whitelisted_caller};
 use frame_system::RawOrigin;
+use totem_primitives::accounting::*;
+
+fn assert_last_event<T: Config>(generic_event: <T as Config>::RuntimeEvent) {
+	frame_system::Pallet::<T>::assert_last_event(generic_event.into());
+}
+
+fn construct_adjustment_details<Balance: Clone>(ledger: Ledger, credit_amount: Balance, debit_amount: Balance) -> Vec<AdjustmentDetail<Balance>>{
+	let mut adjustment_details = vec![];
+
+	let adjustment_detail_credit = AdjustmentDetail {
+		ledger,
+		debit_credit: Indicator::Credit,
+		amount: credit_amount
+	};
+
+	let adjustment_detail_debit = AdjustmentDetail {
+		ledger,
+		debit_credit: Indicator::Debit,
+		amount: debit_amount
+	};
+
+	adjustment_details.push(adjustment_detail_credit);
+	adjustment_details.push(adjustment_detail_debit);
+
+	adjustment_details
+}
 
 benchmarks! {
-	do_something {
-		let s in 0 .. 100;
-		let caller: T::AccountId = whitelisted_caller();
-	}: _(RawOrigin::Signed(caller), s)
+	set_accounting_ref_date {
+		let account: T::AccountId = whitelisted_caller();
+	}: _(RawOrigin::Signed(account.clone()), 500400u32.into())
 	verify {
-		assert_eq!(Something::<T>::get(), Some(s));
+		assert!(AccountingRefDate::<T>::get(&account).is_some());
 	}
 }
 
